7bc2720b17eb1027d3ff1146a0550d91a0af8e4a
Aspectious <gdsiebert@outlook.com>
2025-02-27 20:48:27 -0500

    haha funny message go brr

--- src/components/pages/construction.js
+++ src/components/pages/construction.js
@@ -1,9 +1,26 @@
 function Construction() {
     return(
         <div style={{backgroundColor: "#282c34", padding: "50px", border: "5px solid white"}}>
-            <p>hark this website is under construction</p>
-            <p>come back later</p>
-        </div>
+            <span>
+                <p style={{float: "left"}}>Feburary 27th, 2025</p>
+                <p style={{float: "right"}}>aspectious.dev</p>
+            </span>
+            <br/>
+            <br/>
+            <br/>
+            <div style={{float: "left"}}>
+                <p style={{float: "left"}}>Dear Valued Client,</p>
+                <br/>
+                <p>We regret to inform you that this website is currently unavailable.<br/> It is currently still being built in a dev environment, and will be fully pushed to production when ready.</p>
+                <p>To put it in words used in an earlier commit,<br/>"hark this website is under construction, come back later"</p>
+                <p>Sincerely,</p>
+                <p>Aspectious<br/>Webmaster, aspectious.dev</p>
+            </div>
+            <br/>
+            <br/>
+            <br/>
+            <p style={{textAlign: "center", fontSize: 24}}>If you have any questions about this policy, sorry.</p>
+            </div>
     );
 }
 
